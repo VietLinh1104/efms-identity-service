ea19e8c3949359d625a7943b1873827f50bdbace
OS.NGS-MINHDQ <os.ngs-minhdq@vietinbank.vn>
2026-03-28 18:59:28 +0700

    first commit

---
+++ src/main/java/com/linhdv/efms_identity_service/EfmsIdentityServiceApplication.java
@@ -0,0 +1,14 @@
+package com.linhdv.efms_identity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfmsIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+		SpringApplication.run(EfmsIdentityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linhdv/efms_identity_service/EfmsIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhdv.efms_identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfmsIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
