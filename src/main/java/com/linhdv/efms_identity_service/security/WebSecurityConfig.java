26e24730eb7132c39c6a748d8da9b3836813e1a3
OS.NGS-MINHDQ <os.ngs-minhdq@vietinbank.vn>
2026-04-18 17:51:38 +0700

    [feat] internal service call (UserInfo) cors

--- src/main/java/com/linhdv/efms_identity_service/security/WebSecurityConfig.java
+++ src/main/java/com/linhdv/efms_identity_service/security/WebSecurityConfig.java
@@ -61,6 +61,7 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/api/identity/auth/**").permitAll()
+                        .requestMatchers("/internal/users/batch").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
